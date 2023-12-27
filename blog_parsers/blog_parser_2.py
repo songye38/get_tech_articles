78c15f750ca2074932eed24093e9e44e020b0949
songye38 <ddev0308@gmail.com>
2023-12-27 11:56:07 +0900

    Update blog_parser_2.py 포스트 id로 글 내용 가져오고 csv에 저장하기

--- blog_parsers/blog_parser_2.py
+++ blog_parsers/blog_parser_2.py
@@ -1,6 +1,7 @@
 import urllib.request
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
+from my_utils.save_file import save_to_csv
 
 # 넷플렉스 블로그 글 가져오기
 
@@ -27,9 +28,36 @@ def parse_blog_2(today, diff_days):
     diff = today - datetime_obj
     if abs(diff) <= timedelta(days=diff_days):
         print("The date difference is %d days or less." % diff_days)
+        #글 가져오기 함수 호출 클래스 이름으로
+        post_url = main_div.find('a')['href']
+        get_articles(post_url)
     else:
         print("The date difference is more than %d days" % diff_days)
         return
+    
+def get_articles(article_url):
+    final_text =[]
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+
+    # 초기 세팅
+    try:
+        req = urllib.request.Request(article_url, headers=headers)
+        response = urllib.request.urlopen(req)
+        html = response.read().decode('utf-8')
+    except urllib.error.HTTPError as e:
+        print(f"HTTP Error {e.code}: {e.reason}")
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    main_texts = soup.find_all(class_='pw-post-body-paragraph')
+    for texts in main_texts:
+        final_text.append(texts.text)
+        
+    final_text = " ".join(final_text)
+    save_to_csv(final_text)
+
+
+
 
 
 
