59bab470b72840946ae439ca2e93f237de5165a0
songye38 <ddev0308@gmail.com>
2023-12-27 11:27:07 +0900

    Update blog_parser_2.py 특정 날짜를 기준으로 분기처리

--- blog_parsers/blog_parser_2.py
+++ blog_parsers/blog_parser_2.py
@@ -4,9 +4,8 @@ from datetime import datetime, timedelta
 
 # 넷플렉스 블로그 글 가져오기
 
-def parse_blog_2():
+def parse_blog_2(today, diff_days):
     # 크롤링할 대상 URL
-    today = datetime.today().date()
     url = "https://netflixtechblog.com/?gi=25d9e7be88d3"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
@@ -21,21 +20,36 @@ def parse_blog_2():
     soup = BeautifulSoup(html, 'html.parser')
 
 
-    elements_with_u_flexCenter = soup.find_all(class_='u-flexCenter')
+    main_div = soup.find('div', class_='col u-xs-marginBottom10 u-paddingLeft9 u-paddingRight12 u-paddingTop0 u-sm-paddingTop20 u-paddingBottom25 u-size4of12 u-xs-size12of12 u-marginBottom30')
+    time_div = main_div.find('time')
+    datetime_str = time_div['datetime']
+    datetime_obj = datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S.%fZ").date()
+    diff = today - datetime_obj
+    if abs(diff) <= timedelta(days=diff_days):
+        print("The date difference is %d days or less." % diff_days)
+    else:
+        print("The date difference is more than %d days" % diff_days)
+        return
 
-    for element in elements_with_u_flexCenter:
-    # Find the first 'date' element within each 'u-flexCenter' element
-        date_element = element.find('time')
+
+
+
+
+
+
+    # for element in elements_with_u_flexCenter:
+    # # Find the first 'date' element within each 'u-flexCenter' element
+    #     date_element = element.find('time')
         
-        # Check if date_element is not None before accessing attributes
-        if date_element:
-            datetime_str = date_element['datetime']
+    #     # Check if date_element is not None before accessing attributes
+    #     if date_element:
+    #         datetime_str = date_element['datetime']
             
-            # Convert string to datetime object
-            datetime_obj = datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S.%fZ").date()
-            diff = today - datetime_obj
-            if abs(diff) <= timedelta(days=7):
-                print("The date difference is 7 days or less.")
-            else:
-                print("The date difference is more than 7 days.")
+    #         # Convert string to datetime object
+    #         datetime_obj = datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S.%fZ").date()
+    #         diff = today - datetime_obj
+    #         if abs(diff) <= timedelta(days=7):
+    #             print("The date difference is 7 days or less.")
+    #         else:
+    #             print("The date difference is more than 7 days.")
             
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+
+from datetime import datetime, timedelta
 from blog_parsers.blog_parser_1 import parse_blog_1
 from blog_parsers.blog_parser_2 import parse_blog_2
 
 
 def main():
-    data_blog_2 = parse_blog_2()
+    TODAY = datetime.today().date()
+    DIFF_DAYS = 14
+    parse_blog_2(TODAY,DIFF_DAYS)
 
 if __name__ == "__main__":
     main()
